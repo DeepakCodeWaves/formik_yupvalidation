b87d7ce442523014ebcb9e4364fdfe7278eda7ff
Deepak Verma <141133632+Deepakvermaadequate@users.noreply.github.com>
2023-11-20 15:01:43 +0530

    First Commit
    This is  a form which is made with Formik Library and also Yup Validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,230 @@
-import logo from './logo.svg';
+// import './App.css';
+// import * as Yup from "yup";
+// import { Formik } from 'formik';
+// function App() {
+
+//   const myschema = Yup.object().shape({
+//     firstName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+//     lastName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+//     email: Yup.string().email('Invalid email').required('Required'),
+//   });
+
+//   return (
+//     <div className="App">
+//       Hello This is Formik Form with YUP validation.
+//       <div>
+//         <Formik
+//           initialValues={{ firstName: "", lastName: "", email: "" }}
+//           onSubmit={async values => {
+//             await new Promise(resolve => setTimeout(resolve, 500));
+//             alert(JSON.stringify(values, null, 2));
+//           }}
+//           validationSchema={myschema}
+//         >
+//           {props => {
+//             const {values,touched,errors,dirty,isSubmitting,handleChange,handleBlur,handleSubmit,handleReset} = props;
+//             return (
+//               <form onSubmit={handleSubmit}>
+
+//                 <label htmlFor="firstName" style={{ display: "block" }}>
+//                   First Name
+//                 </label>
+//                 <input
+//                   id="firstName"
+//                   placeholder="Enter your First Name"
+//                   type="text"
+//                   value={values.firstName}
+//                   onChange={handleChange}
+//                   onBlur={handleBlur}
+//                   className={
+//                     errors.firstName && touched.firstName
+//                       ? "text-input error"
+//                       : "text-input"
+//                   }
+//                 />
+//                 {errors.firstName && touched.firstName && (
+//                   <div className="input-feedback">{errors.firstName}</div>
+//                 )}
+
+
+//                 <label htmlFor="lastName" style={{ display: "block" }}>
+//                   Last Name
+//                 </label>
+//                 <input
+//                   id="lastName"
+//                   placeholder="Enter your Last Name"
+//                   type="text"
+//                   value={values.lastName}
+//                   onChange={handleChange}
+//                   onBlur={handleBlur}
+//                   className={
+//                     errors.lastName && touched.lastName
+//                       ? "text-input error"
+//                       : "text-input"
+//                   }
+//                 />
+//                 {errors.lastName && touched.lastName && (
+//                   <div className="input-feedback">{errors.lastName}</div>
+//                 )}
+
+
+//                 <label htmlFor="email" style={{ display: "block" }}>
+//                   Email
+//                 </label>
+//                 <input
+//                   id="email"
+//                   placeholder="Enter your email"
+//                   type="text"
+//                   value={values.email}
+//                   onChange={handleChange}
+//                   onBlur={handleBlur}
+//                   className={
+//                     errors.email && touched.email
+//                       ? "text-input error"
+//                       : "text-input"
+//                   }
+//                 />
+//                 {errors.email && touched.email && (
+//                   <div className="input-feedback">{errors.email}</div>
+//                 )}
+
+//                 <button
+//                   type="button"
+//                   className="outline"
+//                   onClick={handleReset}
+//                   disabled={!dirty || isSubmitting}
+//                 >
+//                   Reset
+//                 </button>
+//                 <button type="submit" disabled={isSubmitting}>
+//                   Submit
+//                 </button>
+
+//               </form>
+//             );
+//           }}
+//         </Formik>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
 
 function App() {
+  const myschema = Yup.object().shape({
+    firstName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+    lastName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Formik Form with YUP Validation</h1>
+      <div className="form-container">
+        <Formik
+          initialValues={{ firstName: '', lastName: '', email: '' }}
+          onSubmit={async (values) => {
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+          validationSchema={myschema}
         >
-          Learn React
-        </a>
-      </header>
+          {(props) => {
+            const {
+              values,
+              touched,
+              errors,
+              dirty,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              handleReset,
+            } = props;
+            return (
+              <form onSubmit={handleSubmit} className="formik-form">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  id="firstName"
+                  placeholder="Enter your First Name"
+                  type="text"
+                  value={values.firstName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.firstName && touched.firstName
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.firstName && touched.firstName && (
+                  <div className="input-feedback">{errors.firstName}</div>
+                )}
+
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  id="lastName"
+                  placeholder="Enter your Last Name"
+                  type="text"
+                  value={values.lastName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.lastName && touched.lastName
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.lastName && touched.lastName && (
+                  <div className="input-feedback">{errors.lastName}</div>
+                )}
+
+                <label htmlFor="email">Email</label>
+                <input
+                  id="email"
+                  placeholder="Enter your email"
+                  type="text"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.email && touched.email
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.email && touched.email && (
+                  <div className="input-feedback">{errors.email}</div>
+                )}
+
+                <div className="button-container">
+                  <button
+                    type="button"
+                    className="outline"
+                    onClick={handleReset}
+                    disabled={!dirty || isSubmitting}
+                  >
+                    Reset
+                  </button>
+                  <button type="submit" disabled={isSubmitting}>
+                    Submit
+                  </button>
+                </div>
+              </form>
+            );
+          }}
+        </Formik>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
